62310e09e67c6f3ec71dae32c6226a1f375bc922
zhudianyu <1462415060@qq.com>
2019-11-04 19:58:32 +0800

    第一篇教程代码

---
+++ Assets/Scripts/01/MyPipelineAsset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental;
+using UnityEngine.Experimental.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/My Pipeline")]
+public class MyPipelineAsset : RenderPipelineAsset
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new MyPippeline();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/01/MyPippeline.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
+public class MyPippeline : RenderPipeline
+{
+    CullResults cull;
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = "Render Camera"
+    };
+
+    Material errorMaterial;
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+        foreach(var cam in cameras)
+        {
+            Render(renderContext, cam);
+        }
+      
+    }
+    void Render(ScriptableRenderContext rendercontext, Camera cam)
+    {
+         ScriptableCullingParameters cullingParameters;
+        //设置剔除参数
+       if(!CullResults.GetCullingParameters(cam, out cullingParameters))
+        {
+            return;
+        }
+      
+        
+        rendercontext.SetupCameraProperties(cam);
+      
+        CameraClearFlags clearFlags = cam.clearFlags;
+        buffer.BeginSample("Render Camera");
+        buffer.ClearRenderTarget((clearFlags&CameraClearFlags.Depth) != 0, (clearFlags&CameraClearFlags.Color)!= 0, cam.backgroundColor);
+        //把ugui 显示在scene视图
+#if UNITY_EDITOR
+        if (cam.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(cam);
+        }
+#endif
+        // buffer.Release();
+
+        var drawSetting = new DrawRendererSettings(cam,new ShaderPassName("SRPDefaultUnlit"));
+        drawSetting.sorting.flags = SortFlags.CommonOpaque;
+        var filterSetting = new FilterRenderersSettings(true) {
+            renderQueueRange = RenderQueueRange.opaque
+        };
+        cull = CullResults.Cull(ref cullingParameters, rendercontext);
+        rendercontext.DrawRenderers(cull.visibleRenderers, ref drawSetting, filterSetting);
+        rendercontext.DrawSkybox(cam);
+        drawSetting.sorting.flags = SortFlags.CommonTransparent;
+        filterSetting.renderQueueRange = RenderQueueRange.transparent;
+     
+        rendercontext.DrawRenderers(cull.visibleRenderers, ref drawSetting, filterSetting);
+
+        DrawDefaultPipeLine(rendercontext, cam);
+
+
+        buffer.EndSample("Render Camera");
+        rendercontext.ExecuteCommandBuffer(buffer);
+
+        buffer.Clear();
+        rendercontext.Submit();
+
+    }
+    [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+    void DrawDefaultPipeLine(ScriptableRenderContext context,Camera camera)
+    {
+        if(errorMaterial == null)
+        {
+            Shader errorShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errorShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+        var drawSetting = new DrawRendererSettings(
+        
+            camera,new ShaderPassName("ForwardBase")
+        );
+        drawSetting.SetShaderPassName(1, new ShaderPassName("PrepassBase"));
+        drawSetting.SetShaderPassName(2, new ShaderPassName("Always"));
+        drawSetting.SetShaderPassName(3, new ShaderPassName("Vertex"));
+        drawSetting.SetShaderPassName(4, new ShaderPassName("VertexLMRGBM"));
+        drawSetting.SetShaderPassName(5, new ShaderPassName("VertexLM"));
+        drawSetting.SetOverrideMaterial(errorMaterial, 0);
+        var filterSetting = new FilterRenderersSettings(true);
+        context.DrawRenderers(cull.visibleRenderers, ref drawSetting, filterSetting);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
